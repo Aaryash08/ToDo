3417819a447a13bf7670f51eaf8bf69878ea6775
Aaryash08 <113716203+Aaryash08@users.noreply.github.com>
2024-03-31 22:16:55 +0530

    Update Server.js

--- backend/Server.js
+++ backend/Server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 mongoose
   .connect(process.env.MONGO_URI)
